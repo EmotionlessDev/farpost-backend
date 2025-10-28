eb6af0b9792814a2854ce4b1325e0b44cf1d02a8
EmotionlessDev <emotionlesscode@gmail.com>
2025-10-28 15:11:21 +1000

    Refactor entitis & fix Dockerfile

--- src/Repository/CityRepository.php
+++ src/Repository/CityRepository.php
@@ -2,39 +2,39 @@
 
 namespace App\Repository;
 
-use App\Entity\ManagingCompany;
+use App\Entity\City;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<ManagingCompany>
+ * @extends ServiceEntityRepository<City>
  */
-class ManagingCompanyRepository extends ServiceEntityRepository
+class CityRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, ManagingCompany::class);
+        parent::__construct($registry, City::class);
     }
 
     //    /**
-    //     * @return ManagingCompany[] Returns an array of ManagingCompany objects
+    //     * @return City[] Returns an array of City objects
     //     */
     //    public function findByExampleField($value): array
     //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
     //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
+    //            ->orderBy('c.id', 'ASC')
     //            ->setMaxResults(10)
     //            ->getQuery()
     //            ->getResult()
     //        ;
     //    }
 
-    //    public function findOneBySomeField($value): ?ManagingCompany
+    //    public function findOneBySomeField($value): ?City
     //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
     //            ->setParameter('val', $value)
     //            ->getQuery()
     //            ->getOneOrNullResult()
